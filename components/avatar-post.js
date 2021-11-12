176e6837ada1ea93a23f1165418bf70a6a127e5d
Santiago Galindo <sp.galindoh@gmail.com>
2021-11-12 16:28:03 -0500

    Custom Follow Author

--- components/avatar-post.js
+++ components/avatar-post.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import { BiCommentDetail } from "react-icons/bi";
 
 import Date from "@/components/date";
 
 export default function AvatarPost({ name, picture, date, postId }) {
   return (
     <>
-      <div className="grid grid-flow-col grid-rows-2 h-12 w-[480px] mb-2">
-        <div className="row-span-2 col-span-1">
+      <div className="grid grid-cols-7 w-96">
+        <div className="col-span-1">
           <Image
             className="rounded-full"
             src={picture.url}
@@ -16,27 +17,38 @@ export default function AvatarPost({ name, picture, date, postId }) {
             layout="fixed"
           />
         </div>
-        <div className="col-span-1 text-gray-800 text-base font-semibold">
-          {name} |{" "}
-          <a href="#vf-conversations" className="hover:underline">
-            <vf-conversations-count vf-container-id={postId} /> comments
-          </a>
-        </div>
-        <div className="col-span-1 text-gray-800 text-base">
-          <Date dateString={date} />
-        </div>
-        <div className="col-span-1 mt-[3px]">
-          <div className="viafoura">
-            <vf-topic-follow
-              topic-id={name}
-              topic-name={name}
-              topic-type="author"
-              show-count="true"
-              minimum-count="5"
-            ></vf-topic-follow>
+        <div className="col-span-6">
+          <div className="flex text-gray-800">
+            <div className="font-semibold">{name} |</div>
+            <div className="text-base ml-1">
+              <Date dateString={date} />
+            </div>
+          </div>
+          <div className="flex text-gray-800">
+            <a
+              className="flex mr-3 hover:underline"
+              href="#vf-conversations"
+              alt="Join the Conversation"
+              title="Join the Conversation"
+            >
+              <BiCommentDetail className="w-5 h-5 mt-[3px] mr-[3px]" />{" "}
+              <vf-conversations-count vf-container-id={postId} />
+            </a>
+            <div className="mt-[3px] ml-1">
+              <div className="viafoura">
+                <vf-topic-follow
+                  topic-id={name}
+                  topic-name={name}
+                  topic-type="author"
+                  show-count="false"
+                  minimum-count="5"
+                ></vf-topic-follow>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
       <div className="mb-10">
         <div className="viafoura">
           <div
@@ -44,7 +56,7 @@ export default function AvatarPost({ name, picture, date, postId }) {
             data-widget="sharebar"
             data-button-view="false"
             data-show-labels="false"
-            data-show-counters="true"
+            data-show-counters="false"
             data-show-total="true"
             data-path="/"
           ></div>
