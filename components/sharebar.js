c9460bffa516cc6237b02fda45abfa37e28a8245
Rob Carling <robcarling@gmail.com>
2021-12-01 16:26:15 -0500

    Mobile sharebar attempt 2

--- components/sharebar.js
+++ components/sharebar.js
@@ -1,15 +1,38 @@
 export default function ShareBar({ showTotal }) {
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 1100);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 1100);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+  
   return (
     <div className="viafoura">
-      <div
-        className="vf-widget vf-share-bar-circle vf-share-bar-horizontal sm:vf-share-bar-vertical"
-        data-widget="sharebar"
-        data-button-view="false"
-        data-show-labels="false"
-        data-show-counters="false"
-        data-show-total={ showTotal }
-        data-path="/"
-      ></div>
+      {isDesktop ? (
+        <div
+          className="vf-widget vf-share-bar-circle vf-share-bar-vertical"
+          data-widget="sharebar"
+          data-button-view="false"
+          data-show-labels="false"
+          data-show-counters="false"
+          data-show-total={ showTotal }
+          data-path="/"
+        ></div>
+      ) : (
+        <div
+          className="vf-widget vf-share-bar-circle vf-share-bar-horizontal"
+          data-widget="sharebar"
+          data-button-view="false"
+          data-show-labels="false"
+          data-show-counters="false"
+          data-show-total={ showTotal }
+          data-path="/"
+        ></div>
+      )}
     </div>
   );
 }
