58a1cf7907f42fbff6021510f552406f57790d14
Santiago Galindo <sp.galindoh@gmail.com>
2021-12-02 10:02:18 -0500

    Dynamic Copyright Year

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ export default function Footer(preview) {
   return (
     <footer className="text-gray-400 text-sm text-center py-7 bg-gray-50 mt-14 border-t border-gray-300">
       <Alert preview={preview} />
-      <div>Viafoura - Copyright © 2021</div>
+      <div>Viafoura - Copyright © {new Date().getFullYear()}</div>
     </footer>
   );
 }
