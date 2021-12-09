c10daf92b9fccd1e68fb685f82d46308c66ac5a9
Santiago Galindo <sp.galindoh@gmail.com>
2021-12-09 09:30:19 -0500

    Topic Navigation

--- components/topic-navigation.js
+++ components/topic-navigation.js
@@ -1,9 +1,8 @@
 export default function TopicNavigation({ allTopics }) {
   return (
-    <div className="flex items-center justify-center h-14 text-gray-500 bg-gray-100 uppercase text-sm border-b-[1px] border-gray-200">
-      Topics:{" "}
+    <div className="flex items-center justify-center -ml-5 h-14 text-gray-500 bg-gray-100 uppercase text-sm border-b-[1px] border-gray-200">
       {allTopics.map((topic) => (
-        <span className="pl-5 font-semibold text-gray-600" key={topic.id}>
+        <span className="ml-5 font-semibold text-gray-600" key={topic.id}>
           {topic.name}
         </span>
       ))}
