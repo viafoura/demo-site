0a4df82453069308fe5ca983fe28e63a27f5b60b
Santiago Galindo <sp.galindoh@gmail.com>
2021-12-22 12:21:28 -0500

    New Entry Point

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ export default class MyDocument extends Document {
           <script
             type="text/javascript"
             async
-            src="//cdn.viafoura.net/vf-v2.js"
+            src="//cdn.viafoura.net/front/0.0.17-PR-32-13-6-df6c1f7/index.js"
           ></script>
         </body>
       </Html>
