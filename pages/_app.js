02e6428aa974031381c77bc31b0af719ece02cd0
robcarling <robcarling@gmail.com>
2022-02-28 11:07:56 -0500

    Revert "TI-294: Auth0 Custom Cookie Login"
    This reverts commit 9a0b4c69bd17e5816810d002cc69a08ead911945.

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,26 @@
 import "@/styles/globals.css";
 
-import { UserProvider } from "@auth0/nextjs-auth0";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import Script from "next/script";
 
 import Layout from "@/components/layout";
-import vfCustomCookieLogin from "@/lib/viafoura/vfCustomCookieLogin";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  const path = router.asPath;
 
   return (
     <>
       <Head>
-        <meta property="og:url" content={`https://${process.env.vfDomain}${path}`} />
+        <meta property="og:url" content={`https://${process.env.vfDomain}${router.asPath}`} />
         <meta name="vf:domain" content={process.env.vfDomain} />
         <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <Script src="/scripts/darkMode.js" strategy="beforeInteractive" />
-      <Script
-        src="https://cdn.viafoura.net/vf-v2.js"
-        strategy="lazyOnload"
-        onLoad={() => {
-          vfCustomCookieLogin(path);
-        }}
-      />
+      <Script src="https://cdn.viafoura.net/vf-v2.js" strategy="lazyOnload" />
       <Layout>
-        <UserProvider>
-          <Component {...pageProps} />
-        </UserProvider>
+        <Component {...pageProps} />
       </Layout>
     </>
   );
