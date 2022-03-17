ad8792413e853c19f4b9e734ab724a3fc96d57dc
Santiago Galindo <sp.galindoh@gmail.com>
2022-03-17 13:01:38 -0500

    TI-313: OIDC Auth0 Improvements

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,22 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin, handleLogout } from "@auth0/nextjs-auth0";
 
-export default handleAuth();
+export default handleAuth({
+  async login(req, res) {
+    try {
+      await handleLogin(req, res, {
+        returnTo: req.headers.referer,
+      });
+    } catch (err) {
+      res.status(err.status || 400).end(err.message);
+    }
+  },
+  async logout(req, res) {
+    try {
+      await handleLogout(req, res, {
+        returnTo: req.headers.referer,
+      });
+    } catch (err) {
+      res.status(err.status || 400).end(err.message);
+    }
+  },
+});
