ca60c20b8484bd0fdb26b2ae42ea47d1ee150c32
lobosan <sp.galindoh@gmail.com>
2024-05-22 11:56:04 -0500

    TI-636: Add Privacy Policy

--- components/footer.js
+++ components/footer.js
@@ -4,6 +4,15 @@ export default function Footer() {
   return (
     <footer className="mt-14 border-t border-gray-300 bg-gray-50 py-7 text-center text-sm text-gray-400 dark:border-neutral-700 dark:bg-[#1A1A1A]">
       <div>Viafoura - Copyright © {new Date().getFullYear()}</div>
+      <div>
+        <a
+          className="privacy-policy"
+          href="https://viafoura.com/privacy-policy"
+          target="_blank"
+        >
+          Privacy Policy
+        </a>
+      </div>
       <DarkModeToggle />
     </footer>
   );
