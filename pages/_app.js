8d15ffac887b0da32d3ef557003edf2cdbf68d55
lobosan <sp.galindoh@gmail.com>
2024-08-05 11:08:26 -0500

    TI-687: Upadate OneSignal Implementation

--- pages/_app.js
+++ pages/_app.js
@@ -25,27 +25,6 @@ export default function MyApp({ Component, pageProps }) {
         />
       </Head>
       <Script src="//cdn.viafoura.net/entry/index.js" onReady={settings} />
-      <Script
-        src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
-        strategy="afterInteractive"
-        onReady={() => {
-          window.OneSignalDeferred = window.OneSignalDeferred || [];
-          OneSignalDeferred.push(async function (OneSignal) {
-            await OneSignal.init({
-              appId: "8add46ba-1535-4c77-8c97-4faccd2cd7e5",
-            });
-          });
-          window.vfQ = window.vfQ || [];
-          window.vfQ.push(async () => {
-            const { user_privilege, id } = await window.vf.context.get("user");
-            if (user_privilege !== "guest") {
-              await OneSignal.login(id.toString());
-            } else {
-              await OneSignal.logout();
-            }
-          });
-        }}
-      />
       <Layout>
         {query.oidc ? (
           <UserProvider>
