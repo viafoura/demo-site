075a640db1726dbbef594c426401b0b674a9ac30
Martin De Simone <martindesimone481@gmail.com>
2026-05-26 13:17:58 -0300

    Add LiveQA to standalone poll demo

---
+++ components/viafoura/vf-liveqa.js
@@ -0,0 +1,7 @@
+export default function VfLiveqa({ vfContainerId }) {
+  return (
+    <div id="vf-livequestions" className="viafoura">
+      <vf-livequestions vf-container-id={vfContainerId} />
+    </div>
+  );
+}
